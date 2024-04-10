b2614e9171c33cc87976c9395373a6cb7af23dd6
Juliana Moreira <omoreiraju@gmail.com>
2024-04-09 21:56:53 -0300

    Refatoração

--- Program.cs
+++ Program.cs
@@ -1,21 +1,35 @@
 ﻿using System.Reflection.Metadata.Ecma335;
 
-double CalculaMetro(int qtd, double tempo){
-    double umpigas = 0.08;
-    double metro = qtd*umpigas*tempo;
-    return metro;
-}
-
-
-
+//Entrada
 Console.WriteLine("Já parou para pensar quantos metros de cigarro você já fumou na vida?");
 
 Console.Write("Quantos cigarros você fuma por dia?");
 int qtd = int.Parse(Console.ReadLine());
-Console.Write("Há quanto tempo você fuma na sua vida em dias?");
+Console.Write("Há quanto tempo você fuma na sua vida em meses?");
 double tempo = double.Parse(Console.ReadLine());
 
-Console.WriteLine($"Com base nas informações passadas, você já fumou {CalculaMetro(qtd, tempo)} metros de cigarro durante a sua vida. \nBora refletir?");
+//Processamento
+double CalculaMetroPigas(int qtd, double tempo){
+    return qtd*0.08*tempo*30;
+}
+
+double CalculaQtdPigas(int qtd, double tempo){
+    return qtd*30*tempo;
+}
+
+double CalculaGastosPigas(int qtd, double tempo){
+    return 8.50/20*qtd*tempo*30;
+}
+
+double CalculaMacosPigas(int qtd, double tempo){
+
+    return qtd*30*tempo/20;
+}
+
+
+//Saída
+Console.Clear();
+Console.WriteLine($"Com base nas informações passadas, você já fumou {CalculaQtdPigas(qtd, tempo)} unidades de cigarros ,\nOque totaliza {CalculaMetroPigas(qtd, tempo)} metros de cigarro fumado durante todo esse tempo. \nO valor que você gastou nessa brincadeira é {CalculaGastosPigas(qtd,tempo):c}, \n valor correspondente a {CalculaMacosPigas(qtd,tempo)} maços de cigarro. \nBora refletir? \n#FumaNãoPo #SingaroDáCãrse");
 
 
 Console.WriteLine("Pressione qualquer tecla para encerrar");
